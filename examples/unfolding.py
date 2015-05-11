5384069d1a622f74ce35389abc0e68cb88a34e03
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2015-05-11 17:01:31 +0200

    return integrated splines after fit

--- examples/unfolding.py
+++ examples/unfolding.py
@@ -10,25 +10,26 @@ class Blobel():
 
     def __init__(
         self,
-        bins_measured,
-        bins_true,
+        n_bins_measured,
+        n_bins_true,
         range_measured,
         range_true,
         n_knots,
     ):
-        self.bins_measured = bins_measured
-        self.bins_true = bins_true
+        self.n_bins_measured = n_bins_measured
+        self.n_bins_true = n_bins_true
         self.range_measured = range_measured
         self.range_true = range_true
         self.n_knots = n_knots
+        self.spline_degree = 4
 
         self.knots = np.linspace(range_true[0], range_true[1], n_knots)
-        self.knots = np.concatenate([np.ones(3)*range_true[0], self.knots, np.ones(3) * range_true[1]])
+        self.knots = np.concatenate([np.ones(self.spline_degree)*range_true[0], self.knots, np.ones(self.spline_degree) * range_true[1]])
 
-        self.n_knots = len(self.knots) - 4
+        self.n_knots = len(self.knots) - self.spline_degree - 1
 
         def splinefunction(x, coefficients):
-            return splev(x, (self.knots, coefficients, 3))
+            return splev(x, (self.knots, coefficients, self.spline_degree))
 
         self.splinefunction = splinefunction
 
@@ -40,11 +41,11 @@ class Blobel():
 
     def fit(self, measured, true):
 
-        response_matrix = np.empty((self.bins_measured, self.n_knots))
+        response_matrix = np.empty((self.n_bins_measured, self.n_knots))
 
         for j in range(self.n_knots):
             entries, _ = np.histogram(measured,
-                                      self.bins_measured,
+                                      self.n_bins_measured,
                                       self.range_measured,
                                       weights=self.singlespline(true, j),
                                       )
@@ -56,7 +57,7 @@ class Blobel():
 
         self.entries_, edges = np.histogram(
             measured,
-            self.bins_measured,
+            self.n_bins_measured,
             self.range_measured,
         )
 
@@ -76,22 +77,64 @@ class Blobel():
                           **kwargs
                           )
 
-        return result
+        self.minimize_result_ = result
+        self.spline_coefficients_ = result.x
+
+        unfolded = np.empty(self.n_bins_true)
+
+        edges = np.linspace(self.range_true[0],
+                            self.range_true[1],
+                            self.n_bins_true + 1,
+                            )
+
+        def result_spline_(x):
+            return self.splinefunction(x, self.spline_coefficients_)
+        self.result_spline_ = result_spline_
+
+        for i, (a, b) in enumerate(zip(edges[:-1], edges[1:])):
+            unfolded[i], _ = quad(result_spline_, a, b)
+
+        return unfolded
 
 
 if __name__ == '__main__':
-    from exampledata import blobel_example
 
-    bias = lambda x: x
-    sigma = 0.1
+    data = np.random.exponential(1, 100000)
+    # data = np.random.uniform(0, 2, int(1e6))
 
-    meas, true = blobel_example(1000000, bias=bias, sigma=sigma)
+    truee = Blobel(
+        n_bins_measured=40,
+        n_bins_true=10,
+        range_measured=[0, 4],
+        range_true=[0, 4],
+        n_knots=5,
+    )
 
-    truee = Blobel(20, 10, [0, 2], [0, 2], 10)
+    truee.fit(data, data)
+    unfolded = truee.predict(data)
 
-    truee.fit(meas, true)
+    plt.matshow(truee.response_matrix_)
+    plt.colorbar()
+    plt.show()
 
-    meas, true = blobel_example(1000000, bias=bias, sigma=sigma)
+    data, edges = np.histogram(data,
+                               bins=truee.n_bins_true,
+                               range=truee.range_true,
+                               )
 
-    result = truee.predict(meas)
+    bin_width = edges[1] - edges[0]
 
+    print('num_splines:', len(truee.spline_coefficients_))
+
+    for i in range(truee.n_knots):
+        print(quad(lambda x: truee.singlespline(x, i), 0, 3))
+
+    plt.bar(edges[:-1], data, bin_width, label='data', alpha=0.3, lw=0, color='red')
+    plt.bar(edges[:-1], unfolded, bin_width, label='unfolded', alpha=0.3, lw=0, color='blue')
+    plt.legend()
+
+    print('Num Events Data:', data.sum())
+    print('Num Events Unfolding:', int(unfolded.sum()))
+    print('ratio:', data.sum()/unfolded.sum())
+
+    plt.show()

a2a1e761189a6d12772548a37b483635309e2d05
Kai <kai.bruegge@gmail.com>
2015-05-12 17:44:00 +0200

    Plotting splines and theri basis functions

--- examples/splines.py
+++ examples/splines.py
@@ -1,31 +1,43 @@
 import numpy as np
 import scipy.interpolate as si
+
 import matplotlib.pyplot as plt
+plt.style.use('ggplot')
 
 N = 20
 degree = 3
 
+# create some sample data and plot it
 x = np.linspace(0, 2*np.pi, N)
 y = np.sin(x) + np.random.normal(0, 0.1, N)
+plt.plot(x, y, "rx", label="Sampled Data")
 
-knots = np.linspace(0, 2*np.pi, 14)[1:-1]
+# define knots and fit a spline to the data
+n_internal_knots = 8
+internal_knots = np.linspace(0, 2*np.pi, n_internal_knots + 2)[1:-1]
+tck = si.splrep(x, y, t=internal_knots, k=degree)
 
-tck = si.splrep(x, y, k=degree, w=0.1*np.ones(N), t=knots)
-tcklist = list(tck)
 
-print(len(tck[0]))
-print(len(tck[1]))
-
+# plot the new spline as well
 px = np.linspace(0, 2*np.pi, 1000)
-plt.plot(x, y, "rx")
-plt.plot(px, si.splev(px, tck), "b-")
-plt.plot(px, np.sin(px), "r--")
+plt.plot(px, si.splev(px, tck=tck), "b-", label="Spline fitted to sample data")
+plt.plot(px, np.sin(px), "--", color="black")
 
-for i in range(len(tck[1])):
-    coeffs = np.zeros_like(tck[1])
-    coeffs[i] = tck[1][i]
+# now plot b-spline basis functions and save them in a list
+knots, coefficients, degree = tck
+basis_functions = []
+for i in range(len(coefficients)):
+    basis_coeffs = np.zeros_like(coefficients)
+    basis_coeffs[i] = coefficients[i]
+    basis_functions.append(si.splev(px, (knots, basis_coeffs, degree)))
+    plt.plot(px, si.splev(px, (knots, basis_coeffs, degree)), color="0.7")
 
-    plt.plot(px, si.splev(px, (tck[0], coeffs, tck[2])))
+basis_functions = np.array(basis_functions)
+plt.plot(px, basis_functions.sum(axis=0), color="orange", linestyle='dashed',  label="Sum of basis functions")
 
+plt.ylim([-1.5, 1.5])
+plt.legend()
+plt.title("Visualization of splines")
 plt.show()
+
 

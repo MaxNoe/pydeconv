2d54d98f7c8f6e6d279c230eebb91655f7b572f5
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2015-05-12 14:10:31 +0200

    update unfolding example

--- examples/unfolding.py
+++ examples/unfolding.py
@@ -11,18 +11,18 @@ from deconv import Blobel
 
 if __name__ == '__main__':
 
-    data = np.random.exponential(1, 100000)
-    # data = np.random.uniform(0, 2, int(1e6))
+    # data = np.random.exponential(1, 100000)
+    data = np.random.uniform(0, 4, int(1e7))
 
     truee = Blobel(
-        n_bins_measured=40,
-        n_bins_true=20,
-        range_measured=[0, 4],
-        range_true=[0, 4],
+        n_bins_observed=40,
+        n_bins_target=20,
+        range_observed=[0, 4],
+        range_target=[0, 4],
         n_knots=10,
     )
-
     truee.fit(data, data)
+
     unfolded = truee.predict(data)
 
     plt.matshow(truee.response_matrix_)
@@ -30,17 +30,22 @@ if __name__ == '__main__':
     plt.show()
 
     data, edges = np.histogram(data,
-                               bins=truee.n_bins_true,
-                               range=truee.range_true,
+                               bins=truee.n_bins_target,
+                               range=truee.range_target,
                                )
 
     bin_width = edges[1] - edges[0]
 
-    print('num_splines:', len(truee.spline_coefficients_))
+    # scale histograms so that area under curve = n_entries
+    data /= bin_width
+    unfolded /= bin_width
 
-    for i in range(truee.n_knots):
-        print(quad(lambda x: truee.singlespline(x, i), 0, 3))
+    px = np.linspace(0, 4, 1000)
 
+    for i, coeff in enumerate(truee.spline_coefficients_):
+        plt.plot(px, truee.singlespline(px, i) * coeff)
+
+    plt.plot(px, truee.result_spline_(px))
     plt.bar(edges[:-1], data, bin_width, label='data', alpha=0.3, lw=0, color='red')
     plt.bar(edges[:-1], unfolded, bin_width, label='unfolded', alpha=0.3, lw=0, color='blue')
     plt.legend()

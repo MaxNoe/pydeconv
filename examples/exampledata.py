1bd77a1acfde924f0dcfb14fc03bb9c7b569c435
Kai <kai.bruegge@gmail.com>
2015-05-12 16:26:32 +0200

    Add more example data

--- examples/exampledata.py
+++ examples/exampledata.py
@@ -1,7 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-def blobel_example(N, bias='blobel', sigma=0.1):
+
+def blobel_example(N, bias='blobel', detector_eff='blobel',  sigma=0.1):
 
     true = np.random.uniform(0, 2, N)
     if bias == 'blobel':
@@ -9,12 +9,15 @@ def blobel_example(N, bias='blobel', sigma=0.1):
     else:
         with_bias = bias(true)
 
-    eff = 1 - 0.5 * (true - 1)**2
-
     if sigma is not None:
-        with_smearing = with_bias + np.random.normal(0, 0.1, N)
+        with_smearing = with_bias + np.random.normal(0, sigma, N)
     else:
         with_smearing = with_bias
+
+    if detector_eff == 'blobel':
+        eff = 1 - 0.5 * (true - 1)**2
+    else:
+        eff = detector_eff(true)
 
     eff_mask = np.random.random(N) < eff
     geom_mask = np.logical_and(with_smearing >= 0, with_smearing <= 2)
@@ -23,3 +26,16 @@ def blobel_example(N, bias='blobel', sigma=0.1):
     true = true[np.logical_and(geom_mask, eff_mask)]
 
     return measured, true
+
+
+def double_gauss(N, sigma_left=0.1, sigma_right=0.2, smearing_sigma=0.1):
+
+    true = np.random.uniform(0, 0.5, N)
+    true[0:N/2] += np.random.normal(0.5, sigma_left, N/2)
+    true[N/2:] += np.random.normal(1.2, sigma_right, N/2)
+
+    measured = true + np.random.normal(0, smearing_sigma, N)
+    measured += -0.1*(1-true**2)
+    return measured, true
+
+
